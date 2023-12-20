3b3337d17064c6ab0cf2a3eca4256036591f5292
Anveks <vekselmana@gmail.com>
2023-12-20 08:45:57 +0200

    demo page of a banking app starter

---
+++ banking app arrays/script.js
@@ -0,0 +1,31 @@
+
+const account1 = {
+  owner: "Anna Smith",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111
+};
+
+const account2 = {
+  owner: "John Doe",
+  movements: [1000, -500, 300, -250, -100, 800, -350],
+  interestRate: 1.5,
+  pin: 2222
+};
+
+const account3 = {
+  owner: "Emily Johnson",
+  movements: [300, -200, 50, 400, -300, 600, -100, 200],
+  interestRate: 1.0,
+  pin: 3333
+};
+
+const account4 = {
+  owner: "Michael Brown",
+  movements: [-200, 100, -300, 500, -150, 1200, -700],
+  interestRate: 1.8,
+  pin: 4444
+};
+
+const accounts = [account1,  account2, account3, account4];
+
