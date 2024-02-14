c4e8a06ff2a02b5121df41ec21557bd45f12901b
Anveks <vekselmana@gmail.com>
2024-02-14 11:58:02 +0200

    more algorithms

--- algorithms/easy/diff two arrays/script2.js
+++ algorithms/easy/diff two arrays/script2.js
@@ -35,11 +35,10 @@ function flattenArray(arr) {
 flattenArray([1, [2, [3]], 4]); // Output: [1, 2, 3, 4]
 
 // 3. Validate US Telephone Numbers
-
 function isValidPhoneNumber(number) {
   const regex = /[\s()\-]/;
   return regex.test(number);
-}
+};
 
 isValidPhoneNumber("1 555-555-5555"); // Output: true
 isValidPhoneNumber("555-555-5555");   // Output: true
@@ -59,4 +58,86 @@ function longestPalindrome(str) {
   }
 }
 
-console.log(longestPalindrome("abacdfgdcaba"));
+longestPalindrome("abacdfgdcaba");
+
+// 5. add digits
+const addDigits = function(num) {
+  if (num < 10) return num;
+
+  const digArr = num.toString().split("");
+  let sum = 0;
+
+  for (let i = 0; i < digArr.length; i++) {
+    sum += Number(digArr[i]);
+  }
+
+  return addDigits(sum);
+};
+
+console.log(addDigits(32312));
+
+  // const sum = digArr.reduce((acc, curr) => acc + Number(curr), 0);
+
+  // if (String(sum).length !== 1) return addDigits(sum);
+  // else return sum;
+
+// 6. ugly number https://leetcode.com/problems/ugly-number/
+var isUgly = function(n) {
+    
+};
+
+// 7. fib sequence
+function fib(num) {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    if (i <= 1) {
+      arr.push(i);
+    } else {
+      const sum = arr[i-1] + arr[i-2];
+      arr.push(sum);
+    }
+  };
+
+  return arr;
+};
+
+fib(5);
+
+// 8. prime factors
+function primerFactors(num) {
+  let prime = 2;
+  const factorsArr = [];
+
+  while (num > 1) {
+    if (num % prime === 0) {
+      factorsArr.push(prime);
+      num = num / prime;
+    } else {
+      prime++;
+    }
+  }
+
+  return factorsArr;
+};
+
+console.log(primerFactors(600));
+
+// 9. longest substring without repeating characters
+const longestSubstring = str => {
+  const hash = {};
+  let substr = "";
+
+  for (const letter of str) {
+    if (!hash.hasOwnProperty(letter)) {
+      hash[letter] = 1;
+      substr = substr.concat(letter);
+    } else {
+      console.log(substr);
+      return substr.length;
+    }
+  }
+
+};
+
+console.log(longestSubstring('bbb'));
